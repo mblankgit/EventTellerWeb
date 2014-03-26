241a4274930f45d0926ab26265d5e9a053b6cc7d
xiaoke1991 <xiaoke_1991@126.com>
2014-03-26 11:40:30 +0800

    update

--- WebRoot/public/js/timeline.js
+++ WebRoot/public/js/timeline.js
@@ -1,10 +1,10 @@
 $(function(){
 	createStoryJS({
         type:       'timeline',
-        //width:      '800',
-        height:     '600',
+        height:     '800',
         //source:     'path_to_json/or_link_to_googlespreadsheet',
         embed_id:   'my-timeline',
+        lang: 'zh-cn',
         css:  'public/css/timeline.css',     //OPTIONAL PATH TO CSS
         js: 'public/js/timeline-min.js'    //OPTIONAL PATH TO JS
     });
